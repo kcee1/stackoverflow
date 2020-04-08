b28f544de189c2c41004ef4adab40d2213ef19a8
evans kelechi <evanskcee@gmail.com>
2020-04-08 04:47:46 -0700

    Add project files.

---
+++ stackoverflow/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace stackoverflow
+{ 
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1, "evans");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+
+            var order = new order();
+            customer.Orders.Add(order);
+        }
+    }
+}
---
+++ stackoverflow/customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stackoverflow
+{
+    class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<order> Orders;
+
+
+        public Customer()
+        {
+            Orders = new List<order>();
+        }
+
+        public Customer(int id)
+        { 
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+
+        }
+
+     
+
+    }
+}
